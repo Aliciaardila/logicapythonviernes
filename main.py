82f5cbf23fbb64b21570700b71c490d2d1bf75a3
Aliciaardila <82722681+Aliciaardila@users.noreply.github.com>
2022-02-25 20:57:02 -0500

    Se crea ejemplo de 3 condiciones

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 #comentario
 
 nivelAgua=int(input("Digita el nivel del agua de la represa:"))
-print(f"el nivel de agua es {nivelAgua}")
-print("hola")
+if(nivelAgua<200):
+    print("No tengo agua")
+elif (nivelAgua >=200 and nivelAgua <450):
+    print("todo bien, energia corriendo")
+else:
+    print("ojo con población aguas abajo")
